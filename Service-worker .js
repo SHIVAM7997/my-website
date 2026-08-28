aa6472303221fe54593c39aadeb15c842395b1f0
SHIVAM7997 <skm328388@gmail.com>
2026-08-28 20:43:14 +0530

    Update service worker to use new cache version

--- Service-worker .js
+++ Service-worker .js
@@ -1,28 +1,27 @@
-const CACHE_NAME = "zevacare-v1";
+const CACHE_NAME = "zevacare-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./dashboard.html",
   "./health-guide.html",
-  "./health-report.html",
-  "./manifest.json"
+  "./health-report.html"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then((keys) =>
       Promise.all(
-        keys.map(key => {
+        keys.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
@@ -33,11 +32,10 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-/* Internet available ho to latest website use hogi */
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
+    fetch(event.request)
+      .then((response) => response)
+      .catch(() => caches.match(event.request))
   );
 });
